d2c0adf31d8e74817d2c34c486c1746768701dfb
Alpaslan Avcı <alpaslan.avci@std.yeditepe.edu.tr>
2024-11-26 21:26:13 +0300

    Update heatSystems_control.v

--- heatSystems_control.v
+++ heatSystems_control.v
@@ -1,12 +1,12 @@
 `include "project.v"
 
-module heatSystems_control (input wire [1:0] mode, input wire mode_select, output selected_mode);
+module heatSystems_control (input wire [1:0] mode, input wire mode_select, output reg selected_mode);
     //There is a 2to1 Multiplexer for the present 2 modes of heating systems. According to select inputs
     //one of the mods will be selected.
 
     wire mux_out;       
     mux2to1 heatSystems_mode_selector(.sel(mode_select), .in(mode), .out(mux_out));
     always @(*)
-        select_mode = mux_out;
+        selected_mode = mux_out;
 
 endmodule

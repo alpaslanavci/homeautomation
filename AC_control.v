d9dde9be518bf875f6e52564829985834ed39ff3
Alpaslan Avcı <alpaslan.avci@std.yeditepe.edu.tr>
2024-11-26 21:25:52 +0300

    Update AC_control.v

--- AC_control.v
+++ AC_control.v
@@ -1,6 +1,6 @@
 `include "project.v"
 
-module AC_control(input wire [1:0] mode_selection, input wire [3:0] mode, output selected_mode);
+module AC_control(input wire [1:0] mode_selection, input wire [3:0] mode, output reg selected_mode);
     //There is a 4to1 Multiplexer for the present 4 modes of AC. According to select inputs
     //one of the mods will be selected.
 
@@ -8,6 +8,6 @@ module AC_control(input wire [1:0] mode_selection, input wire [3:0] mode, output
     mux4to1 ac_mode_selector(.sel(mode_selection), .in(mode), .out(mux_out));
 
     always @(*)
-        select_mode = mux_out
+        selected_mode = mux_out
 
 endmodule
